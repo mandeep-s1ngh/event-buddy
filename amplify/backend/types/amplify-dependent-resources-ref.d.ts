28a5e3a186fbc588b22c844d83142fa84c9be3a2
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-03-27 19:55:23 +0100

    amplify configured with graphql, react native elements installed

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "eventbuddy": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
