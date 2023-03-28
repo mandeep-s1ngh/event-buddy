bf73212993d769bf38fbcc317b5cb744b4c67101
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-03-28 09:47:55 +0100

    update json5 package

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello event buddies!</Text>
+      <Text>Hello event app buddies!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
