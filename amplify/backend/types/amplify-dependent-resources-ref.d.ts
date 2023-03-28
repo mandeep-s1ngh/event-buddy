d8ee68b93f3f527decbaabbb0f73010d8554e947
Carces <theo_johnson@ntlworld.com>
2023-03-28 12:02:23 +0100

    initial commit - amplify project set up with api removed

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "eventbuddy": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+  "function": {
+    "patchUsersUsername": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
