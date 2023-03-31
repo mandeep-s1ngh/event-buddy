47d3b46895e0776f8e8c39d5bad937b8ca601eab
Farishta7 <farishtatq@gmail.com>
2023-03-31 14:46:52 +0100

    linked both buttons to event list

--- components/LocationRequest.jsx
+++ components/LocationRequest.jsx
@@ -1,28 +1,40 @@
 import { Button } from "@rneui/base";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, TouchableHighlight } from "react-native";
 import styles from "../styles";
+
+import { useNavigation } from '@react-navigation/native';
 
 const LocationRequest = (props) => {
   const { userLocation, setUserLocation } = props;
 
+  const navigation = useNavigation();
+
+  function navigateToEventsList() {
+    navigation.navigate('EventsList');
+  }
+
   return (
     <View style={styles.Location_Request}>
-      <Text style={styles.Location_TextInfo}>
-        We use your location to find events near you. Alternatively, enter your
-        location below:
-      </Text>
-      <TextInput
-        value={userLocation}
-        onChangeText={(userLocation) => {
-          setUserLocation(userLocation);
-        }}
-        style={styles.Location_TextInput}
+
+      <Text style={styles.Location_TextInfo}>We use your location to find events near you.</Text>
+
+      <View style={{paddingTop: 1, paddingBottom: 60} }>
+        <TouchableHighlight  style={styles.Location_Buttons} onPress={navigateToEventsList}>
+          <Text style={styles.Location_Buttons_Text}>Use my location 📍</Text>
+        </TouchableHighlight>
+      </View>
+
+      <Text style={styles.Location_TextInfo}>Alternatively, enter your location below:</Text>
+      <TextInput value={userLocation} onChangeText={(userLocation) => {setUserLocation(userLocation);}} style={styles.Location_TextInput}
         placeholder="Venue, city, zip code"
       />
 
-      <Button style={styles.Location_SubmitButton}>Submit</Button>
+      <View style={{paddingTop: 10, paddingBottom: 10} }>
+        <TouchableHighlight  style={styles.Location_Buttons} onPress={navigateToEventsList}>
+          <Text style={styles.Location_Buttons_Text}>Submit</Text>
+        </TouchableHighlight>
+      </View>
 
-      <Text>You entered this text: {userLocation}</Text>
     </View>
   );
 };
